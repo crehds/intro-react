0283baa07048fd9915ae8fd9b4d2ac313f82fe05
carlos rodriguez <carlos15erh@gmail.com>
2022-02-15 14:10:45 -0500

    handling state

--- src/components/TodoSearch/index.js
+++ src/components/TodoSearch/index.js
@@ -1,14 +1,19 @@
+import { useState } from 'react';
 import './styles.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = useState('');
+
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
 
   return (
     <input
       className='TodoSearch'
       placeholder='cebolla'
+      value={searchValue}
       onChange={onSearchValueChange}
     />
   );
