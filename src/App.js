bb32d9b74343c0931d8ec74ce6c9e7e78e5a0784
carlos rodriguez <carlos15erh@gmail.com>
2022-02-16 17:45:11 -0500

    adding data in the localStorage

--- src/App.js
+++ src/App.js
@@ -3,32 +3,48 @@
 import { useState } from 'react';
 import { AppUI } from './pages/AppUI';
 
-const defaultTodos = [
-  { text: 'Cortar cebolla', completed: true },
-  { text: 'Tomar un curso en Platzi', completed: false },
-  { text: 'Hacer ejercicio', completed: false },
-  { text: 'LALALALA', completed: false }
-];
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: true },
+//   { text: 'Tomar un curso en Platzi', completed: false },
+//   { text: 'Hacer ejercicio', completed: false },
+//   { text: 'LALALALA', completed: false }
+// ];
 
 function App() {
-  const [todos, setTodos] = useState(defaultTodos);
+  const localStorageTodos = localStorage.getItem('TODOS_V1');
+
+  let parsedTodos;
+  if (!localStorageTodos) {
+    localStorage.setItem('TODOS_V1', JSON.stringify([]));
+    parsedTodos = [];
+  } else {
+    parsedTodos = JSON.parse(localStorageTodos);
+  }
+
+  const [todos, setTodos] = useState(parsedTodos);
   const [searchValue, setSearchValue] = useState('');
 
   const completedTodos = todos.filter((todo) => todo.completed).length;
   const totalTodos = todos.length;
 
+  const saveTodos = (newTodos) => {
+    const stringifiedTodos = JSON.stringify(newTodos);
+    localStorage.setItem('TODOS_V1', stringifiedTodos);
+    setTodos(newTodos);
+  };
+
   const completeTodo = (text) => {
-    const updateTodos = defaultTodos.slice();
+    const updateTodos = todos.slice();
     const updateTodo = updateTodos.find((todo) => todo.text === text);
 
     updateTodo.completed = !updateTodo.completed;
 
-    setTodos(updateTodos);
+    saveTodos(updateTodos);
   };
 
   const deleteTodo = (text) => {
-    const deleteTodos = [...defaultTodos].filter((todo) => todo.text !== text);
-    setTodos(deleteTodos);
+    const deleteTodos = [...todos].filter((todo) => todo.text !== text);
+    saveTodos(deleteTodos);
   };
 
   const searchedTodos = todos.filter((todo) =>
