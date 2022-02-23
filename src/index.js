d07a1572576fa114cded3d9a894d3419ab66678e
carlos rodriguez <carlos15erh@gmail.com>
2022-02-22 21:39:18 -0500

    experimenting with HOCs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function App(props) {
+  return (
+    <h1>
+      !{props.saludo}, {props.nombre}
+    </h1>
+  );
+}
+
+function withSaludo(WrapperComponent) {
+  return function WrapperComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <>
+          <WrapperComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrapperComponent</p>
+        </>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = withSaludo(App)('buenitas');
+
+ReactDOM.render(
+  <AppWithSaludo saludo={'Buenas'} nombre={'Carlos'} />,
+  document.getElementById('root')
+);
